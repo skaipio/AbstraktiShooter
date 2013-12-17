1ce56a2f8ba240593159f45624c3d170a314d1f8
Senni Kaipio <senni.kaipio@gmail.com>
2013-12-18 01:15:31 +0200

    Tile map testing

--- AbstraktiShooter-desktop/src/com/abstrakti/shooter/Main.java
+++ AbstraktiShooter-desktop/src/com/abstrakti/shooter/Main.java
@@ -2,9 +2,17 @@ package com.abstrakti.shooter;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
 
 public class Main {
 	public static void main(String[] args) {
+		Settings settings = new Settings();
+		settings.maxHeight = 512;
+		settings.maxWidth = 512;
+		
+		TexturePacker2.process(settings, "textures", "../AbstraktiShooter-android/assets", "textureAtlas");
+		
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "AbstraktiShooter";
 		cfg.useGL20 = false;
--- AbstraktiShooter/src/com/abstrakti/shooter/MainGameScreen.java
+++ AbstraktiShooter/src/com/abstrakti/shooter/MainGameScreen.java
@@ -2,44 +2,57 @@ package com.abstrakti.shooter;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector3;
 
 public class MainGameScreen implements Screen {
 	private OrthographicCamera camera;
+	private OrthogonalTiledMapRenderer renderer;
 	private SpriteBatch batch;
 	private Texture texture;
-	private Sprite sprite;
+	private TiledMap map;
+	private TmxMapLoader loader;
 	
 	public MainGameScreen(SpriteBatch batch){
 		this.batch = batch;
+		this.loader = new TmxMapLoader();
+		this.map = loader.load("testlevel.tmx");
 		
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
-		camera = new OrthographicCamera(1, h/w);
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800/32, 600/32);
+		this.renderer = new OrthogonalTiledMapRenderer(map, 1/32f);
+		this.renderer.setView(camera);
 		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+//		sprite = new Sprite(region);
+//		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+//		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+//		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
 	}
 
 	@Override
-	public void render(float delta) {		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+	public void render(float delta) {	
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		if (Gdx.input.isKeyPressed(Keys.A)){
+			this.camera.translate(-1,0,0);			
+			this.camera.update();
+			System.out.println(this.camera.position);
+		}
+		this.renderer.setView(camera);
+		this.renderer.render();
 	}
 
 	@Override
