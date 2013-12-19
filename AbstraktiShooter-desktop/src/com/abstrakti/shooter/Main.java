f7961dcebd91e4c27d76c9571b7d940ba2e4f78d
vilniemi <vilniemi@gmail.com>
2013-12-19 12:57:24 +0200

    Testi commit

--- AbstraktiShooter-desktop/src/com/abstrakti/shooter/Main.java
+++ AbstraktiShooter-desktop/src/com/abstrakti/shooter/Main.java
@@ -8,9 +8,10 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "AbstraktiShooter";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 640;
+		cfg.height = 480;
 		
 		new LwjglApplication(new Game(), cfg);
 	}
 }
+	
