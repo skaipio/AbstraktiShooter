7095b348ce0812d88c839c6fe9e774f831500ccc
Kaipio Senni K <skaipio@da3-cs-dk110-10.helsinki.fi>
2013-12-23 14:36:21 +0200

    -

--- AbstraktiShooter/src/com/abstrakti/shooter/Level.java
+++ AbstraktiShooter/src/com/abstrakti/shooter/Level.java
@@ -1,20 +1,15 @@
 package com.abstrakti.shooter;
 
-import java.util.List;
-
 import com.abstrakti.shooter.io.GameScreen;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Level {
 	private final World physicsWorld;
 	private TiledMap map;
-	private List<Entity> entities;
+	//private List<Entity> entities;
 	
 	public Level(String levelname){
 		this.physicsWorld = new World(Vector2.Zero, true);		
@@ -24,8 +19,14 @@ public class Level {
 		
 		GameScreen.getInstance().setMap(map);
 		
+		
 //		this.physicsWorld.createBody(new BodyDef());
 //		BodyDef def = new BodyDef();
+	}
+	
+	public void init(){
+//		TiledMapTileLayer layer = (TiledMapTileLayer)this.map.getLayers().get(0);
+//		layer.getCell(0,0).getTile().getProperties().containsKey("collision") == 1 jos true, lisää fysiikka
 	}
 	
 	public void update(){
@@ -33,6 +34,7 @@ public class Level {
 	}
 	
 	public void addEntity(Entity e){
-		this.entities.add(e);
+		// this.entities.add(e);
+		// this.physicsWorld.createBody(def)
 	}
 }
