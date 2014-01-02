edde24ab272408869c019571917c41546d9121d8
Senni Kaipio <senni.kaipio@gmail.com>
2014-01-02 13:14:58 +0200

    Update level to spawn player from a point set in Tiled editor. Rename DynamicObjectFactory.

--- AbstraktiShooter/src/com/abstrakti/shooter/Level.java
+++ AbstraktiShooter/src/com/abstrakti/shooter/Level.java
@@ -1,11 +1,17 @@
 package com.abstrakti.shooter;
 
+import javax.rmi.CORBA.Tie;
+
 import com.abstrakti.shooter.io.GameScreen;
-import com.abstrakti.shooter.objects.DynamicObjectFactory;
+import com.abstrakti.shooter.objects.GameObjectFactory;
 import com.abstrakti.shooter.objects.GameObject;
 import com.abstrakti.shooter.objects.Player;
 import com.abstrakti.shooter.objects.Wall;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
@@ -28,30 +34,18 @@ public class Level {
 	
 	public Level(String levelname){
 		this.physicsWorld = new World(Vector2.Zero, true);	
-		
-		this.player = DynamicObjectFactory.createPlayer(physicsWorld);
-		this.player.setPosition(190, 90);
-				
-		
 		TmxMapLoader loader = new TmxMapLoader();
 		
 		this.map = loader.load(levelname);
 		
-		GameScreen.getInstance().setMap(map);
-		GameScreen.getInstance().lockCameraOn(this.player);
+		GameScreen.getInstance().setMap(map);		
 	}
 	
 	public void init(){
 		TiledMapTileLayer layer = (TiledMapTileLayer)this.map.getLayers().get(0);
-		int tileSize = Config.TILE_SIZE;
-		float tileSizeHalved = tileSize / 2f;
 		
-		PolygonShape boxShape = new PolygonShape();
-        boxShape.setAsBox(tileSizeHalved*Config.WORLD_TO_BOX,tileSizeHalved*Config.WORLD_TO_BOX);
-        
-        BodyDef myBodyDef2 = new BodyDef();
-        myBodyDef2.type = BodyType.StaticBody; //this will be a static$       
-        myBodyDef2.angle = 0; //set the starting angle
+		
+		int tileSize = Config.TILE_SIZE;
 		
 		for (int i=0;i<layer.getWidth();i++) {
 			for (int j=0;j<layer.getHeight();j++) {
@@ -61,22 +55,30 @@ public class Level {
 					continue;
 				}
 				
-				if (cell.getTile().getProperties().get("collision").equals("true") ) {
-								
-					myBodyDef2.position.set(tileSize*i*Config.WORLD_TO_BOX, tileSize*j*Config.WORLD_TO_BOX); //set the starting position
-
-	                Body body2 = physicsWorld.createBody(myBodyDef2);    
-
-	                FixtureDef boxFixtureDef2 = new FixtureDef();
-	                boxFixtureDef2.shape = boxShape;
-	                boxFixtureDef2.density = 1;
-	                body2.createFixture(boxFixtureDef2);
-	                body2.setUserData(new Wall());					
-					
+				if (cell.getTile().getProperties().get("collision").equals("true") ) {								
+					Vector2 position = new Vector2(tileSize*i*Config.WORLD_TO_BOX, tileSize*j*Config.WORLD_TO_BOX);
+					GameObjectFactory.createWall(physicsWorld, position);
 				}
 				
 			}
 		}
+		
+		this.setPlayerIntoLevel();
+	}
+	
+	private void setPlayerIntoLevel(){
+		System.out.println(map.getLayers().getCount());
+		MapLayer triggerLayer = map.getLayers().get("Spawns");
+		MapObjects mapObjects = triggerLayer.getObjects();		
+		MapObject mapObject = mapObjects.get("playerSpawn");
+		MapProperties properties = mapObject.getProperties();
+		int x = (Integer)properties.get("x");
+		int y = (Integer)properties.get("y");
+		
+		this.player = GameObjectFactory.createPlayer(physicsWorld);
+		this.player.setPosition(x, y);
+		
+		GameScreen.getInstance().lockCameraOn(this.player);
 	}
 	
 	public void update(float deltaTime){
--- AbstraktiShooter/src/com/abstrakti/shooter/objects/GameObjectFactory.java
+++ AbstraktiShooter/src/com/abstrakti/shooter/objects/GameObjectFactory.java
@@ -4,14 +4,16 @@ import com.abstrakti.shooter.Config;
 import com.abstrakti.shooter.animations.PlayerIdleAnimation;
 import com.abstrakti.shooter.animations.PlayerWalkAnimation;
 import com.abstrakti.shooter.animations.SpriteAnimation;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
-public final class DynamicObjectFactory {
+public final class GameObjectFactory {
 	public static Player createPlayer(World world){
 		Player player = new Player();
 		SpriteAnimation playerWalking = new PlayerWalkAnimation(player);
@@ -41,5 +43,29 @@ public final class DynamicObjectFactory {
 		shape.dispose();
 		
 		return player;
+	}
+	
+	public static void createWall(World world, Vector2 position){
+		int tileSize = Config.TILE_SIZE;
+		float tileSizeHalved = tileSize / 2f;
+		
+		PolygonShape boxShape = new PolygonShape();
+        boxShape.setAsBox(tileSizeHalved*Config.WORLD_TO_BOX,tileSizeHalved*Config.WORLD_TO_BOX);
+        
+        BodyDef myBodyDef2 = new BodyDef();
+        myBodyDef2.type = BodyType.StaticBody; //this will be a static$       
+        myBodyDef2.angle = 0; //set the starting angle
+        
+        myBodyDef2.position.set(position); //set the starting position
+
+        Body body2 = world.createBody(myBodyDef2);    
+
+        FixtureDef boxFixtureDef2 = new FixtureDef();
+        boxFixtureDef2.shape = boxShape;
+        boxFixtureDef2.density = 1;
+        body2.createFixture(boxFixtureDef2);
+        body2.setUserData(new Wall());				
+        
+        boxShape.dispose();
 	}
 }
