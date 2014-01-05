a4f6198eb7fb75c39504387525cd1094f77bdc55
larrxi <lars.pekkala@helsinki.fi>
2014-01-05 13:31:36 +0200

    gun sound

--- AbstraktiShooter/src/com/abstrakti/shooter/objects/Weapon.java
+++ AbstraktiShooter/src/com/abstrakti/shooter/objects/Weapon.java
@@ -1,14 +1,19 @@
 package com.abstrakti.shooter.objects;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
 public class Weapon {
 	private WeaponFiremode firemode;
 	private boolean triggerStatus;
 	private int ammo;
 	private int MAXAMMO = 100;
+	private Sound sound;
 	
 	public Weapon(WeaponFiremode firemode) {
 		this.triggerStatus = false;
 		this.firemode = firemode;
+		sound = Gdx.audio.newSound(Gdx.files.internal("barreta_m9-Dion_Stapper-1010051237.wav"));
 	}
 	
 	public int getAmmo() {
@@ -34,6 +39,7 @@ public class Weapon {
 			this.triggerStatus = true;
 			this.ammo--;
 			System.out.println("BANG, ammo" + " " + ammo);
+			sound.play(1.0f);
 			return true;
 		}
 	}
