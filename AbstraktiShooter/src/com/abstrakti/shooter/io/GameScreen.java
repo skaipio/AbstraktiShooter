021aa2c8f4058aa783542e61bd40d0cbd94d48bd
vilniemi@gmail.com <vilniemi@gmail.com>
2014-01-07 15:43:57 +0200

    alpha to ui-bars

--- AbstraktiShooter/src/com/abstrakti/shooter/io/GameScreen.java
+++ AbstraktiShooter/src/com/abstrakti/shooter/io/GameScreen.java
@@ -155,19 +155,23 @@ public class GameScreen implements Screen {
 	}
 	
 	private void drawHealth() {
+		Gdx.gl.glEnable(GL10.GL_BLEND);
+	    Gdx.gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
 		float width = (currentLevel.getPlayer().getHealth()/20F) *120;
 		ShapeRenderer shapeRenderer = new ShapeRenderer();
 		 shapeRenderer.begin(ShapeType.Filled);
-		 shapeRenderer.setColor(1, 0, 0, 0);
+		 shapeRenderer.setColor(1, 0, 0, 0.6f);
 		 shapeRenderer.rect(0, 0, width, 30);
 		 shapeRenderer.end();
 		
 	}
 	private void drawAmmo() {
 		float width = (currentLevel.getPlayer().getAmmo()/100F) *120;
+		Gdx.gl.glEnable(GL10.GL_BLEND);
+	    Gdx.gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
 		ShapeRenderer shapeRenderer = new ShapeRenderer();
 		 shapeRenderer.begin(ShapeType.Filled);
-		 shapeRenderer.setColor(0, 0, 1, 0);
+		 shapeRenderer.setColor(0, 0, 1,  0.6f);
 		 shapeRenderer.rect(1024-120, 0, width, 30);
 		 shapeRenderer.end();
 	}
